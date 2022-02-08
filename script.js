8ca34e434a4a7d68174350df9b4f2ca7636ced25
Als8713 <99030834+Als8713@users.noreply.github.com>
2022-02-07 22:02:00 -0300

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-function Verificar(){ 
-    var op = document.getElementsByName('inlineRadioOptions')
-    if(op[1].checked){   
-        window.alert("Resposta não aceita, responda corretamente!!!");
-    }else{ 
-        window.alert("Só vamoooo");
-    }
-}
